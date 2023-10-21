fb5309c1024fa8520d3bbb82df794b676418c560
Maria Eduarda Benevenutti <115738167+eduardabenevenutti77@users.noreply.github.com>
2023-10-21 17:03:25 -0300

    update

--- js/subpagina.js
+++
@@ -1,8 +0,0 @@
-document.getElementById("vestido").addEventListener("click", function () {
-    var subpagina = document.getElementById("subpagina");
-    subpagina.style.display = "block";
-});
-document.getElementById("fecharSubpagina").addEventListener("click", function () {
-    var subpagina = document.getElementById("subpagina");
-    subpagina.style.display = "none";
-});
